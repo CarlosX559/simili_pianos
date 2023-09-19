dee26d57847e0144e94bea415c94f3fc32098c21
Carlos Xavier <carlos.lipe559@hotmail.com>
2023-09-19 11:34:52 -0300

    send final files

--- enviar.php
+++ enviar.php
@@ -1,51 +1,67 @@
 <?php
-if (!empty($_POST["nome"]) && !empty($_POST["endereco"])) {
+// Content of create_deal.php
 
-    $nome = addslashes($_POST['nome']);
-    $email = addslashes($_POST['email']);
-    $telefone = addslashes($_POST['telefone']);
-    $endereco = addslashes($_POST["endereco"]);
-    $produto = addslashes($_POST["produto"]);
-
-    //$email_to = "contato@similimusica.com.br";
-    $email_to = "carlosxavier.dev@gmail.com";
+function sendDataToPipedrive($api_token, $URL, $data)
+{
 
 
-    $assunto = "Formulário Simili Pianos";
-    
-    $header = 'MIME-Version: 1.1'."\r\n".
-        'Content-type: text/html; charset=utf-8'."\r\n".
-        'From: Simili Pianos <$email_to>'."\r\n".
-        'Return-Path: $email_to'."\r\n".
-        'Reply-To: $email'."\r\n".
-        'X-Mailer: PHP/'.phpversion();
-    
+    $URL = $URL . '?api_token=' . $api_token;
 
-    // Dados que serao enviados.
-    $body = "Nome: ".$nome." "."<br>"."\r\n".
-        "E-mail: ".$email." "."<br>"."\r\n".
-        "Telefone: ".$telefone." "."<br>"."\r\n".
-        "Endereço: ".$endereco." "."<br>"."\r\n".
-        "Produto: ".$produto;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $URL);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-
-    //Enviando o email.
-    $status = mail($email_to, $assunto, $body, $header);
-
-
-    if ($status) {
-        
-        header("Location: https://api.whatsapp.com/send?phone=5547999943130&text=Olá!%20Me%20chamo%20$nome,%20tenho%20interesse%20em%20seu%20Piano%20Yamaha%20exclusivo%20modelo%20$produto%20e%20gostaria%20de%20mais%20informações.");
-           
-        $email_obrigado = $email;
-        $assunto_obrigado = 'Teste obrigado';
-        $body_obrigado = 'Olá! É um prazer poder estar lhe atendendo, agradecemos por entrar em contato conosco e expressar seu interesse em nossos pianos.<br> Nossa equipe entrará em contato em breve para discutir suas necessidades e responder às suas perguntas.<br><br> Obrigado por escolher a Simili Pianos!<br> Enquanto isso, confira nossas redes e conheça mais sobre nós: 📱 Instagram: https://www.instagram.com/simili.pianos/';
-        
-        mail($email_obrigado, $assunto_obrigado, $body_obrigado, $header);
-
-    } /*else {
-       header("Location:contato.php?status=erro");
-
-   }*/
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $result = json_decode($output, true);
 }
-?>
+
+if (!isset($_POST["nome"]))
+    return;
+
+$api_token = '43bdd95e34b3fe0e6a50f088e6185cd269a7150e';
+
+$person = array(
+    'name' => $_POST["nome"],
+    'email' => $_POST["email"],
+    'phone' => $_POST["telefone"],
+    'adress' => $_POST["endereco"],
+    'product' => $_POST["produto"],
+);
+
+$nome = addslashes($_POST['nome']);
+$produto = addslashes($_POST["produto"]);
+
+//AddPerson
+$result = sendDataToPipedrive($api_token, 'https://similimusica.pipedrive.com/v1/persons', $person);
+
+
+// Check if an ID came back, if did print it out
+if (!empty($result['data']['id'])) {
+
+
+    $deal = array(
+        'title' => 'LP PIANO - ' . $_POST["nome"] . " - " . $_POST["email"] . " - " . $_POST["telefone"] . " - " . $_POST["telefone"] . " - " . $_POST["endereco"] . " - " . $_POST["produto"],
+        'org_id' => '1',
+        'person_id' => $result['data']['id']
+    );
+
+
+    $result = sendDataToPipedrive($api_token, 'https://similimusica.pipedrive.com/v1/deals', $deal);
+    echo json_encode(array("return" => true, "id" => $result['data']['id']));
+
+
+
+    header("Location: https://api.whatsapp.com/send?phone=5547999943130&text=Olá!%20Me%20chamo%20$nome,%20tenho%20interesse%20em%20seu%20Piano%20Yamaha%20exclusivo%20modelo%20$produto%20e%20gostaria%20de%20mais%20informações.");
+
+    $email_obrigado = $email;
+    $assunto_obrigado = 'A Simili Pianos agradece seu contato!';
+    $body_obrigado = 'Olá! É um prazer poder estar lhe atendendo, agradecemos por entrar em contato conosco e expressar seu interesse em nossos pianos.<br> Nossa equipe entrará em contato em breve para discutir suas necessidades e responder às suas perguntas.<br><br> Obrigado por escolher a Simili Pianos!<br> Enquanto isso, confira nossas redes e conheça mais sobre nós: 📱 Instagram: https://www.instagram.com/simili.pianos/';
+
+    mail($email_obrigado, $assunto_obrigado, $body_obrigado, $header);
+
+
+}
---
+++ enviar_form.php
@@ -0,0 +1,49 @@
+<?php
+if (!empty($_POST["nome"]) && !empty($_POST["endereco"])) {
+
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
+    $endereco = addslashes($_POST["endereco"]);
+    $produto = addslashes($_POST["produto"]);
+
+    $email_to = "contato@similimusica.com.br";
+    
+    $assunto = "Formulário Simili Pianos";
+    
+    $header = 'MIME-Version: 1.1'."\r\n".
+        'Content-type: text/html; charset=utf-8'."\r\n".
+        'From: Simili Pianos <$email_to>'."\r\n".
+        'Return-Path: $email_to'."\r\n".
+        'Reply-To: $email'."\r\n".
+        'X-Mailer: PHP/'.phpversion();
+    
+
+    // Dados que serao enviados.
+    $body = "Nome: ".$nome." "."<br>"."\r\n".
+        "E-mail: ".$email." "."<br>"."\r\n".
+        "Telefone: ".$telefone." "."<br>"."\r\n".
+        "Endereço: ".$endereco." "."<br>"."\r\n".
+        "Produto: ".$produto;
+
+
+    //Enviando o email.
+    $status = mail($email_to, $assunto, $body, $header);
+
+
+    if ($status) {
+        
+        header("Location: https://api.whatsapp.com/send?phone=5547999943130&text=Olá!%20Me%20chamo%20$nome,%20tenho%20interesse%20em%20seu%20Piano%20Yamaha%20exclusivo%20modelo%20$produto%20e%20gostaria%20de%20mais%20informações.");
+           
+        $email_obrigado = $email;
+        $assunto_obrigado = 'A Simili Pianos agradece seu contato!';
+        $body_obrigado = 'Olá! É um prazer poder estar lhe atendendo, agradecemos por entrar em contato conosco e expressar seu interesse em nossos pianos.<br> Nossa equipe entrará em contato em breve para discutir suas necessidades e responder às suas perguntas.<br><br> Obrigado por escolher a Simili Pianos!<br> Enquanto isso, confira nossas redes e conheça mais sobre nós: 📱 Instagram: https://www.instagram.com/simili.pianos/';
+        
+        mail($email_obrigado, $assunto_obrigado, $body_obrigado, $header);
+
+    } /*else {
+       header("Location:contato.php?status=erro");
+
+   }*/
+}
+?>
